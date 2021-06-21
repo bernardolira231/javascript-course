5c8867c2705667619029b9a9f78eb4a54ca8d780
Bernardo Lira <harritolira@gmail.com>
2021-06-20 23:44:39 -0500

    New excersise

---
+++ bloque1/paroimpar.js
@@ -0,0 +1,8 @@
+var primero = 23;
+var segundo = 2;
+
+if ((primero % segundo) == 0) {
+  console.log(`${primero} es divisible entre ${segundo}`)
+} else {
+  console.log(`${primero} no es divisible entre ${segundo}`)
+}
